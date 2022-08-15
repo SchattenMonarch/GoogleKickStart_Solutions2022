d68feffc08661a37802102490acbe5b55535a3b9
Dennis Folz <folzdennis@gmail.com>
2022-08-15 20:50:26 +0200

    Added a program description

---
+++ Coding Practice with Kick Start Session #1/H-index/H-index.c
@@ -0,0 +1,18 @@
+/*
+Title: Google Kick Start 2022: Coding Practice with Kick Start Session #1
+
+Problem: H-Index
+
+Description: This program calculates the H-Index from the number of papers and number of citations per paper.
+
+Sample Input
+2
+3
+5 1 2
+6
+1 3 3 2 2 15
+
+Sample Output
+Case #1: 1 1 2
+Case #2: 1 1 2 2 2 3
+*/
