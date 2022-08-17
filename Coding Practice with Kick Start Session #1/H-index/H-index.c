da140b7ae05f7d0a1218e4d3961a9237e013f572
Dennis Folz <folzdennis@gmail.com>
2022-08-17 02:37:07 +0200

    removed the code for the brute force approach. Will work on more time efficient approach. I will use a min heap.

--- Coding Practice with Kick Start Session #1/H-index/H-index.c
+++ Coding Practice with Kick Start Session #1/H-index/H-index.c
@@ -22,43 +22,12 @@ Case #1: 1 1 2
 Case #2: 1 1 2 2 2 3
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int cit[10000] = { 0 };
-
-int score(int paper_number, int ctr);
-
+#define TRUE 1
+#define FALSE 0
 int main()
 {
-	int tests = 0, papers = 0, tmp = 0, h_index = 0;
-	scanf("%d", &tests);
-	for (int t = 1; t <= tests; t++) {
-		scanf("%d", &papers);
-		for (int p = 0; p < papers; p++) {
-			scanf("%d", &cit[p]);
-		}
-		printf("Case #%d: ", t);
-		for (int i = 1; i <= papers; i++) {
-			for (int j = i; j >= h_index; j--) {
-				tmp = score(j, i);
-				if (tmp > h_index && tmp >= j) {
-					h_index = j;
-					break;
-				}
-				
-			}
-			printf("%d ", h_index);
-		}
-		h_index = 0;
-		tmp = 0;
-	}
-
 	return 0;
-}
-
-int score(int paper_number, int ctr) {
-	int ret = 0, cpy = paper_number;
-	for (int i = 0; i < ctr; i++) {
-		ret += cit[i] >= paper_number ? 1 : 0;
-	}
-	return ret;
 }
