2afaa28830dbafd2e6734095715e6d2365cd64e2
Dennis Folz <folzdennis@gmail.com>
2022-08-17 04:54:35 +0200

    adjusted the program declarations and removed the trivial comments for better overview

--- Coding Practice with Kick Start Session #1/H-index/H-index.c
+++ Coding Practice with Kick Start Session #1/H-index/H-index.c
@@ -9,6 +9,7 @@ Title: Google Kick Start 2022: Coding Practice with Kick Start Session #1
 Problem: H-Index
 
 Description: This program calculates the H-Index from the number of papers and number of citations per paper.
+			 For a good time complexity = Nlog(N) a min heap data structure will be implemented.
 
 Sample Input
 2
@@ -25,28 +26,41 @@ Case #2: 1 1 2 2 2 3
 #include <stdlib.h>
 #include <limits.h>
 
-#define TRUE 1
-#define FALSE 0
+#define TRUE  ((unsigned char) 1u)
+#define FALSE ((unsigned char) 0u)
 
 typedef struct min_heap
 {
-    int* p;
+    int* item;
     int size;
     int count;
 } Heap;
 
-Heap* create_heap(void); /*Creates a min_heap structure and returns a
-                                  pointer to the struct*/
-void down_heapify(Heap* heap, int index); /*Pushes an element downwards in the
-                                             heap to find its correct position*/
-void up_heapify(Heap* heap, int index); /*Pushes an element upwards in the heap
-                                           to find its correct position*/
-void push(Heap* heap, int x);           /*Inserts an element in the heap*/
-void pop(Heap* heap); /*Removes the top element from the heap and adjusts the shape of the min heap*/
-int top(Heap* heap); /*Returns the top element of the heap or returns INT_MIN if
-                        heap is empty*/
-int empty(Heap* heap); /*Checks if heap is empty*/
-int size(Heap* heap);  /*Returns number of elements in the heap*/
+Heap* create_heap(void); 
+
+void heapify_up(Heap* heap, int index);
+void heapify_down(Heap* heap, int index); 
+
+void push(Heap* heap, int x);        
+void pop(Heap* heap); 
+unsigned char swap(Heap* heap, int index_one, int index_two); 
+
+int get_size(Heap* heap);  
+int get_top(Heap* heap); 
+
+int get_parent_index(Heap* heap, int child_index);
+int get_left_child_index(Heap* heap, int parent_index);
+int get_right_child_index(Heap* heap, int parent_index);
+
+int get_left_child(Heap* heap, int index);
+int get_right_child(Heap* heap, int index);
+int get_parent(Heap* heap, int index);
+
+unsigned char is_empty(Heap* heap); 
+unsigned char has_parent(Heap* heap, int index);    
+unsigned char has_left_child(Heap* heap, int index); 
+unsigned char has_right_child(Heap* heap, int index);  
+
 
 int main()
 {
@@ -58,7 +72,8 @@ Heap* create_heap(void)
 	//allocate memory for the heap and its elements
     Heap* heap = (Heap*)malloc(sizeof(Heap));
     heap->size = 1;
-    heap->p = (int*)malloc(heap->size * sizeof(int));
+    heap->item = (int*)malloc(heap->size * sizeof(int));
     heap->count = 0;
     return heap;
 }
+
