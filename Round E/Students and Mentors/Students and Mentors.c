fe9bef844685f0fd58958b3f1368ce4cfc186490
Dennis Folz <folzdennis@gmail.com>
2022-08-22 20:27:51 +0200

    Implemented merge_sorted_arrays

--- Round E/Students and Mentors/Students and Mentors.c
+++ Round E/Students and Mentors/Students and Mentors.c
@@ -66,3 +66,31 @@ void merge_sort_recursion(ll a[], ll left, ll right) {
 	}
 	return;
 }
+
+void merge_sorted_arrays(ll a[], ll left, ll middle, ll right) {
+	ll left_length = middle + 1 - left, right_length = right - middle, i = 0u, j = 0u, k = 0u;
+
+	ll* left_arr = (ll*)malloc(left_length * sizeof(ll));
+	ll* right_arr = (ll*)malloc(right_length * sizeof(ll));
+
+	for (i = 0; i < left_length; i++) {
+		left_arr[i] = a[left + i];
+	}
+	for (i = 0; i < right_length; i++) {
+		right_arr[i] = a[middle + 1 + i];
+	}
+
+	for (i = 0, j = 0, k = left; k <= right; k++) {
+		// i not at the end of the left array AND (j finished the right array OR i at left array i smaller than j at right array)
+		if (i < left_length
+			&& (j >= right_length || left_arr[i] <= right_arr[j])) {
+			a[k] = left_arr[i++];
+		}
+		else {
+			a[k] = right_arr[j++];
+		}
+	}
+	free(left_arr);
+	free(right_arr);
+	return;
+}
