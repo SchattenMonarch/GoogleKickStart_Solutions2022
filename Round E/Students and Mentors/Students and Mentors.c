78bbe8875122793d7bffe7489fd3d00936dcd1f6
Dennis Folz <folzdennis@gmail.com>
2022-08-22 20:21:53 +0200

    Created Student and Mentors.c Added program description Added libraries, defines and function prototypes

---
+++ Round E/Students and Mentors/Students and Mentors.c
@@ -0,0 +1,28 @@
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   21.08.2022
+last update: 22.08.2022
+
+program: google kick start Round E: Students and Mentors
+link: https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb0f5/0000000000ba84ae
+
+Sample
+
+Sample Input
+
+3
+3
+2000 1500 1900
+5
+1000 600 1000 2300 1800
+2
+2500 1200
+
+Sample Output
+
+Case #1: 1900 2000 2000
+Case #2: 1800 1000 1800 1800 2300
+Case #3: 1200 -1
+
+*/
