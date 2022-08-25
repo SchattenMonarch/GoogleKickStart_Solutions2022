7d5a867ab0e4392b71fd6cd80c18f1de434b40df
Dennis Folz <folzdennis@gmail.com>
2022-08-25 18:39:36 +0200

    created pizza-delivery.c and added program description and author information

---
+++ Round E/Pizza Delivery/pizza-delivery.c
@@ -0,0 +1,58 @@
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   23.08.2022
+last update: 24.08.2022
+program: google kick start Round E: Pizza Delivery
+link: https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb0f5/0000000000ba86e6
+Sample
+Test Set 1: P = 0
+
+Sample Input
+2
+3 0 1 1 2
++ 1
+- 2
++ 3
+/ 4
+3 0 1 2 3
+- 2
+- 2
+- 2
+- 2
+
+Sample Output
+Case #1: 3
+Case #2: 0
+
+Test Set 2: 0 <= P <= 10
+
+Sample input
+3
+3 1 3 1 3
++ 4
+- 2
+* 1
+/ 4
+1 2 4
+2 2 1 1 2
++ 2
++ 3
+* 2
+* 1
+1 1 4
+2 2 1
+3 1 2 1 3
++ 1
+* 1
+- 3
+/ 4
+2 2 2
+
+Sample Output
+
+Case #1: 8
+Case #2: IMPOSSIBLE
+Case #3: 1
+
+*/
