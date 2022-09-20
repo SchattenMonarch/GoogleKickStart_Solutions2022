297ab1a0d98b0788141be90197db2fcce8a58256
Dennis Folz <folzdennis@gmail.com>
2022-09-20 12:18:04 +0200

    Created new Folder for Round F Created new Folder for the Problem "Sort the Fabrics" with an empty .md file and a .c file with a program description

---
+++ Round F/Sort the Fabrics/Sort the Fabrics.c
@@ -0,0 +1,16 @@
+/*
+author: Dennis Folz
+github profile : https://github.com/SchattenMonarch
+date:   20.09.2022
+last update: 20.09.2022
+program: google kick start Round F: Sort the Fabrics
+link: https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb409/0000000000beefbb
+
+TODO: add sample
+Sample
+
+Sample Input
+
+Sample Output
+
+*/
